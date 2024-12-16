504095ddcde1102b4724d9171759f66f9cd96e6a
Akash Kumar <codewithpanda28@gmail.com>
2024-12-16 20:25:28 +0530

    nothing change

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import Link from "next/link";
 import emailjs from '@emailjs/browser';
 
+
 export default function ContactPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
